855a1c53a7a5d57632170e6950b2995c42f89514
sgtzang <kohla@daum.net>
2015-08-21 17:18:09 +0900

    Lesson1 Complete

--- app/src/main/java/com/example/kohla/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/kohla/sunshine/MainActivityFragment.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -23,17 +28,27 @@ public class MainActivityFragment extends Fragment {
 
         String[] forecastArray = {
                 "Today - Sunny - 88/63",
-                "Tommorow - Foggy - 70/40",
+                "Tomorrow - Foggy - 70/40",
                 "Weds - Cloudy - 72/63",
                 "Thurs - Asteroids - 75/65",
                 "Fri - Heavy Rain - 65/56",
-                "Sat - Help Trapped in weatherstation - 60/51",
+                "Sat - Help Trapped in weather station - 60/51",
                 "Sun - Sunny - 80/68"
         };
 
-        List<String> weekForecast = new Array<String>(
-                Arrays.asList(forecastArray)
-        );
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> weekForecast = new ArrayList<String>( Arrays.asList(forecastArray)  );
+
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        weekForecast);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
